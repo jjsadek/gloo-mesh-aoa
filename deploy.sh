d4b5846fd2537ceeb00be8dbe876056e5f856d6a
Alex <alexbrucely@gmail.com>
2022-01-20 11:04:02 -0800

    provide a temp key

--- deploy.sh
+++ deploy.sh
@@ -1,26 +1,14 @@
 #!/bin/bash
-
-license_key=$1
 cluster1_context="cluster1"
 cluster2_context="cluster2"
 mgmt_context="mgmt"
-gloo_mesh_overlay="1-2-7"
 meshctl_version="v1.2.7"
-istio_overlay="1-11-4"
 
 # check to see if defined contexts exist
 if [[ $(kubectl config get-contexts | grep ${mgmt_context}) == "" ]] || [[ $(kubectl config get-contexts | grep ${cluster1_context}) == "" ]] || [[ $(kubectl config get-contexts | grep ${cluster2_context}) == "" ]]; then
   echo "Check Failed: Either mgmt, cluster1, and cluster2 contexts do not exist. Please check to see if you have three clusters available"
   echo "Run 'kubectl config get-contexts' to see currently available contexts. If the clusters are available, please make sure that they are named correctly. Default is mgmt, cluster1, and cluster2"
   exit 1;
-fi
-
-# check to see if license key variable was passed through, if not prompt for key
-if [[ ${license_key} == "" ]]
-  then
-    # provide license key
-    echo "Please provide your Gloo Mesh Enterprise License Key:"
-    read license_key
 fi
 
 # install argocd on mgmt, ${cluster1_context}, and ${cluster2_context}
@@ -35,7 +23,7 @@ cd argocd
 ../tools/wait-for-rollout.sh deployment argocd-server argocd 10 ${cluster2_context}
 
 # deploy mgmt environment apps aoa
-kubectl apply -f environments/mgmt/meta/meta-mgmt-env-apps.yaml
+kubectl apply -f environments/mgmt/meta/meta-mgmt-env-apps.yaml --context ${mgmt_context}
 # use later
 #kubectl apply -f environments/mgmt/meta/meta-mgmt-env-config.yaml
 
